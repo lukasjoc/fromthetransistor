5c021045ff57a201b5d398df45caec7255e654e9
lukasjoc <jochamlu@gmail.com>
2021-04-17 16:40:40 +0200

    use more stuffs

--- hardware/blinking_led/blinking_led.srcs/constrs_1/new/blinking_zybo7.xdc
+++ hardware/blinking_led/blinking_led.srcs/constrs_1/new/blinking_zybo7.xdc
@@ -9,7 +9,18 @@ set_property -dict { PACKAGE_PIN K17   IOSTANDARD LVCMOS33 } [get_ports { i_clk 
 create_clock -add -name sys_clk_pin -period 8.00 -waveform {0 4} [get_ports { i_clk }];
 
 ## Switches
-set_property -dict { PACKAGE_PIN G15   IOSTANDARD LVCMOS33 } [get_ports { i_sw1 }]; #IO_L19N_T3_VREF_35 Sch=sw[0]
+set_property -dict { PACKAGE_PIN G15   IOSTANDARD LVCMOS33 } [get_ports { i_sw0 }]; #IO_L19N_T3_VREF_35 Sch=sw[0]
+set_property -dict { PACKAGE_PIN P15   IOSTANDARD LVCMOS33 } [get_ports { i_sw1 }]; #IO_L24P_T3_34 Sch=sw[1]
+set_property -dict { PACKAGE_PIN W13   IOSTANDARD LVCMOS33 } [get_ports { i_sw2 }]; #IO_L4N_T0_34 Sch=sw[2]
+set_property -dict { PACKAGE_PIN T16   IOSTANDARD LVCMOS33 } [get_ports { i_sw3 }]; #IO_L9P_T1_DQS_34 Sch=sw[3]
 
 ## LED (non rgb)
-set_property -dict { PACKAGE_PIN M14    IOSTANDARD LVCMOS33 } [get_ports { o_led }]; #IO_L19P_T3_13 Sch=led5_g
+set_property -dict { PACKAGE_PIN M14   IOSTANDARD LVCMOS33 } [get_ports { o_led0 }]; #IO_L23P_T3_35 Sch=led[0]
+set_property -dict { PACKAGE_PIN M15   IOSTANDARD LVCMOS33 } [get_ports { o_led1 }]; #IO_L23N_T3_35 Sch=led[1]
+set_property -dict { PACKAGE_PIN G14   IOSTANDARD LVCMOS33 } [get_ports { o_led2 }]; #IO_L19P_T3_13 Sch=led5_g
+set_property -dict { PACKAGE_PIN D18   IOSTANDARD LVCMOS33 } [get_ports { o_led3 }]; #IO_L3N_T0_DQS_AD1N_35 Sch=led[3]
+
+## RGB LED 5 (Zybo Z7-20 only)
+set_property -dict { PACKAGE_PIN Y11   IOSTANDARD LVCMOS33 } [get_ports { o_led_r }]; #IO_L18N_T2_13 Sch=led5_r
+set_property -dict { PACKAGE_PIN T5    IOSTANDARD LVCMOS33 } [get_ports { o_led_g }]; #IO_L19P_T3_13 Sch=led5_g
+set_property -dict { PACKAGE_PIN Y12   IOSTANDARD LVCMOS33 } [get_ports { o_led_b }]; #IO_L20P_T3_13 Sch=led5_b
--- hardware/blinking_led/blinking_led.srcs/sources_1/new/top.sv
+++ hardware/blinking_led/blinking_led.srcs/sources_1/new/top.sv
@@ -1,31 +1,58 @@
 `timescale 1ns / 1ps
 
-// TOOD: blink led with switch of button
 // TODO: rgb blinking controlled by what type of switch is turned on
 // all turned on led blinks in red green and blue and so on
-module top(input i_clk, i_sw1, output o_led);
-    // led_blink inst_led_blink(i_clk, o_led);
-    on_sw_led_blink inst_on_sw_led_blink(i_clk, i_sw1, o_led);
+module top(
+    input
+        i_clk, // the clock
+        i_sw0, i_sw1, i_sw2, i_sw3, // all users switches
+    output
+        o_led0, o_led1, o_led2, o_led3, // all user leds
+        o_led_r, o_led_g, o_led_b // one rgb led
+    );
+   
+// rgb_blink_sw_control inst_rgb_blink_sw_control(
+//      i_clk,
+//      i_sw0, i_sw1, i_sw2,
+//      i_led_r, i_led_g, i_led_b
+// );
+
+    led_blink inst_led_blink(i_clk, o_led1);
+    led_blink_odd led_blink_odd(i_clk, o_led2);
+    on_sw_led_blink inst_on_sw_led_blink(i_clk, i_sw3, o_led3);
 endmodule
 
-module led_blink( input i_clk, output [1:0]o_led );
-reg on_bit = 1'b1;
-reg [32:0]cnt;
-reg state;
-assign o_led[0] = state;
-assign o_led[1] = on_bit;
-always @(posedge i_clk) begin
+// just blink at 2^24 around every third of a second
+module led_blink(input i_clk, output [1:0]o_led );
+reg o_led = 1'b1;
+reg [31:0]cnt;
+always @(posedge i_clk ) begin
     cnt <= cnt +1'b1;
-    state <= cnt[24];
+    o_led <= cnt[24];
 end
 endmodule
 
-module on_sw_led_blink( input i_clk, i_sw1, output [1:0] o_led );
+// blink at odd posedges
+module led_blink_odd(input i_clk, output [1:0]o_led );
+reg [31:0] cnt;
+reg o_led = 1'b0;
+reg state = 1'b0;
+always @(posedge i_clk) begin
+    state <= state + 1'b1;
+    if(state % 2 != 0) begin
+        cnt <= cnt +1'b1;
+        o_led <= cnt[24];
+    end
+end
+endmodule
+
+// blink if switch is on
+module on_sw_led_blink(input i_clk, i_sw, output [1:0]o_led );
 reg [32:0] cnt;
 reg o_led = 1'b0;
 always @(posedge i_clk) begin
-    if(i_sw1) begin 
-        cnt <= cnt + 1'b1;
+    if(i_sw) begin 
+        cnt <= cnt +1'b1;
         o_led <= cnt[24];
     end else begin
         cnt <= 0;
@@ -33,3 +60,12 @@ always @(posedge i_clk) begin
     end
 end
 endmodule
+
+//module rgb_blink_sw_control(
+//    input
+//        i_clk, i_sw,
+//    output
+//      o_led_r, o_led_g, o_led_b
+//   );
+//endmodule
+
